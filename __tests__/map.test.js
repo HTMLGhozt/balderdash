4b2e097ddbd289ab24c32252975297a6a6407540
Thomas Dillard <thomas.dillard.studios@gmail.com>
2019-03-31 02:30:36 -0400

    Starting testing

--- __tests__/map.test.js
+++ __tests__/map.test.js
@@ -1,10 +1,29 @@
 import { test } from 'tap';
-import _ from '../lib/index';
+import td from 'testdouble';
 
+td.config({ suppressErrors: true });
+
+import map from '../lib/map';
 test('map', ({ test, end }) => {
-  test('It should be a function', t => {
-    t.assert(typeof _.map === 'function');
+  test('should be a function', t => {
+    t.assert(typeof map === 'function');
     t.end();
+  });
+
+  test('should iterate over an array', t => {
+    // const callback = td.function('callback');
+    // const captor = td.matchers.captor();
+    t.ok(true);
+    t.end();
+
+    // TODO: figure out how to best write this test
+
+    // const array = [1, 2, 3, 4];
+    // map(array, callback);
+
+    // td.verify(callback(captor.capture()), { ignoreExtraArgs: true });
+    // td.verify(callback(), { times: 4, ignoreExtraArgs: true });
+    // t.end();
   });
   end();
 });
